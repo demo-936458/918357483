645fadf3b61187075f1c6920b1e9d60ad8ec700b
iman <iman@syvlabs.com>
2017-10-18 13:32:37 +0800

    separate graph config files

--- viz/index.js
+++ viz/index.js
@@ -73,41 +73,8 @@ function constructGraph(response) {
     cy = cytoscape({
         container: $('#canvas'),
         elements: nodes.concat(edges),
-        style: [{
-                selector: 'node',
-                style: {
-                    'font-size': 5,
-                    'width': 7,
-                    'height': 7,
-                    'background-color': '#666',
-                    'label': 'data(name)'
-                }
-            },
-            {
-                selector: 'edge',
-                style: {
-                    'width': 'data(weight)',
-                    'line-color': '#ccc',
-                    'target-arrow-color': '#ccc',
-                    'opacity': 0.4
-                }
-            }
-        ],
-        layout: {
-            name: 'cose',
-            gravity: 1,
-            nodeRepulsion: 400000,
-            idealEdgeLength: function(edge) {
-                // Default is: 10
-                // Instead, base it on "weight"
-                return edge.data().weight * 100
-            },
-            edgeElasticity: function(edge) {
-                // Default is: 100
-                // Instead, base it on "weight"
-                return edge.data().weight * 100
-            },
-        }
+        style: graphStyle,
+        layout: graphLayout
     });
 
 }
